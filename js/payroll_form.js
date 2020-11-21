20689d150dcdaba1c6f5fa7610d132f0578815e5
Mayur <mayurzope555@gmail.com>
2020-11-21 22:25:18 +0530

    [Mayur]Added: Modified Employee Payroll class with new getters and setters attributes

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,75 +1,3 @@
-class EmployeePayrollData {
-  constructor(...params) {
-    this.name = params[0];
-    this.salary = params[1];
-    this.gender = params[2];
-    this.startDate = params[3];
-    this.department = params[4];
-  }
-
-  get name() {
-    return this._name;
-  }
-  set name(name) {
-    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
-    if (nameRegex.test(name)) this._name = name;
-    else throw "Name is incorrect!";
-  }
-  get department() {
-    return this._department;
-  }
-  set department(department) {
-    this._department = department;
-  }
-  get salary() {
-    return this._salary;
-  }
-  set salary(salary) {
-    let salaryRegex = RegExp("^[1-9]{1}[0-9]*$");
-    if (salaryRegex.test(salary)) this._salary = salary;
-    else throw "Salary should be non zero positive number";
-  }
-  get gender() {
-    return this._gender;
-  }
-  set gender(gender) {
-    if (gender != undefined) {
-      let genderRegex = RegExp("^(male|female)$");
-      if (genderRegex.test(gender)) {
-        this._gender = gender;
-      } else {
-        throw "Gender incorrect";
-      }
-    }
-  }
-  get startDate() {
-    return this._startDate;
-  }
-  set startDate(startDate) {
-    if (startDate != undefined) {
-      if (startDate <= new Date()) {
-        const options = { year: "numeric", month: "long", day: "numeric" };
-        const employeeDate = startDate.toLocaleDateString("en-US", options);
-        this._startDate = employeeDate;
-      } else throw " Please select the valid date!";
-    }
-  }
-  toString() {
-    return (
-      " Name: " +
-      this.name +
-      " Salary: " +
-      this.salary +
-      " Gender: " +
-      this.gender +
-      " Start Date: " +
-      this.startDate +
-      "Department: " +
-      this.department
-    );
-  }
-}
-
 const salary = document.querySelector("#salary");
 const output = document.querySelector(".salary-output");
 output.textContent = salary.value;
@@ -86,11 +14,11 @@ const dateError = document.querySelector(".date-error");
     if (month.value == 1) {
       if (isLeapYear(year.value)) {
         if (day.value > 29) {
-          dateError.textContent = "Invalid Date!";
+          dateError.textContent = "Date is Invalid!";
         } else dateError.textContent = "";
       } else {
         if (day.value > 28) {
-          dateError.textContent = "Invalid Date!";
+          dateError.textContent = "Date is Invalid!";
         } else dateError.textContent = "";
       }
     }
@@ -101,7 +29,7 @@ const dateError = document.querySelector(".date-error");
       month.value == 10
     ) {
       if (day.value > 30) {
-        dateError.textContent = "Invalid Date!";
+        dateError.textContent = "Date is Invalid!";
       } else dateError.textContent = "";
     }
   })
@@ -117,6 +45,13 @@ function save() {
     var day = document.querySelector("#day").value;
     var startDate = new Date(year, month, day);
     let department = new Array();
+    const images = document.getElementsByName("profile");
+    let profilePic=images[0];
+    for(let i=0;i<images.length;i++){
+        if(images[i].checked)
+            profilePic=images[i].value;
+    }
+    var note = document.getElementById("notes").value;
     const departmentForm = document.getElementsByClassName("checkbox");
     for (let i = 0; i < departmentForm.length; i++) {
       if (departmentForm[i].checked) department.push(departmentForm[i].value);
@@ -126,7 +61,9 @@ function save() {
       salary,
       gender,
       startDate,
-      department
+      department,
+      note,
+      profilePic
     );
     alert(employee);
   } catch (error) {
